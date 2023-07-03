b95e0289a625e05b108bab497ab0ffee62a3100d
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-07-03 13:52:38 +0530

    Update analog_script.js

--- analog_script.js
+++ analog_script.js
@@ -1,13 +1,20 @@
-setInterval(() => {
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
-    
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-    }, 1000);
+function show_Clock() {
+    let h = document.getElementsByClassName('hr')[0];
+    let m = document.getElementsByClassName('mn')[0];
+    let s = document.getElementsByClassName('ss')[0];
+  
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+  
+    h.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
+    m.style.transform = `rotate(${6 * minutes}deg)`;
+    s.style.transform = `rotate(${6 * seconds}deg)`;
+  
+    let sound = new Audio('sound.mp3');
+    sound.play();
+  }
+  
+  setInterval(show_Clock, 1000);
+  
